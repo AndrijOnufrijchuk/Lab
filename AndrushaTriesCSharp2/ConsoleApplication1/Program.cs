4013bfcee695f0ce7c63679ff12e9ed3cd302f13
Furaw <44235585+Furaw@users.noreply.github.com>
2019-09-16 11:39:47 +0300

    Add files via upload

---
+++ AndrushaTriesCSharp2/ConsoleApplication1/Program.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class RotateandSumm {
+        static int[] Shifter(int[] arr, int k)
+        {
+            k++;
+            int[,] res = new int[k, arr.Length];
+
+            for (int times = 0; times < k; times++)
+            {
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (i - times < 0)
+                    {
+                        res[times, i] = arr[arr.Length - times + i];
+                    }
+                    else
+                    {
+                        res[times, i] = arr[i - times];
+                    }
+                }
+
+            }
+            int[] sum = new int[arr.Length];
+            for (int j = 0; j < sum.Length; j++)
+            {
+                for (int i = 0; i < k; i++)
+                {
+                    sum[j] += res[i, j];
+                }
+            }
+
+            return sum;
+        }
+
+        public RotateandSumm()
+        {
+            Console.WriteLine("Start entering array of 5");
+            int k;
+            int[] array = new int[5];
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                array[i] = int.Parse(Console.ReadLine());
+
+            }
+            Console.Write(" Enter k ");
+            k = Convert.ToInt32(Console.ReadLine());
+            int[] res = Shifter(array, k);
+            for (int i = 0; i < res.Length; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+        }
+    }
+
+    class FoldandSumm {
+        public FoldandSumm() {
+            int k,o=0,p=0;
+            Console.Write(" Enter k ");
+            k = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[4*k];
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                array[i] = int.Parse(Console.ReadLine());
+
+            }
+            
+            int[] array2 = new int[array.Length/2];
+            for (int i = 0; i < array2.Length; i++)
+            {
+                array2[i] = array[i];
+                 o += array2[i];
+            }
+            int[] array3 = new int[array.Length / 2];
+            for (int i = 0; i < array3.Length; i++)
+            {
+                array3[i] = array[ i + array.Length/2];
+                p += array3[i];
+            }
+
+            Console.WriteLine(o+" "+p);
+
+
+
+        }
+
+    }
+    
+    
+     class SieveOfEratosthenes {
+         public SieveOfEratosthenes()
+         {
+
+             int n=0, i, m = 0;
+             Console.Write("Enter number");
+             n = int.Parse(Console.ReadLine());
+
+             m = n / 2;
+             for( ;n>2;n--){    
+             for (i = 2; i <= m; i++)
+                 {
+
+                     if (n % i == 0)
+                     {
+
+                         return;
+                     }
+                     else {
+                         Console.WriteLine(n);
+                     }
+                  }
+         }
+
+
+     }
+     }
+
+     class CompareCharArrays
+     {
+         public CompareCharArrays()
+         {
+
+
+             int k;
+             Console.Write(" Enter k ");
+             k = Convert.ToInt32(Console.ReadLine());
+                   
+             char[] array = new char[k];
+             for (int i = 0; i < array.Length; i++)
+             {
+
+                 array[i] = Console.ReadKey().KeyChar;
+
+             }
+             Console.WriteLine("");
+
+             
+
+            int c;   
+             Console.Write(" Enter c ");
+             c = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("");
+             char[] array2 = new char[c];
+             for (int i = 0; i < array2.Length; i++)
+             {
+
+                 array2[i] = Console.ReadKey().KeyChar;
+
+             }
+             Console.WriteLine("");
+
+             
+             byte[] ASCIIValues = Encoding.ASCII.GetBytes(array);
+             byte[] ASCIIValues2 = Encoding.ASCII.GetBytes(array2);
+
+             foreach (char q in array)
+             {
+               
+
+                 foreach (char z in array2)
+                 {
+
+                     if ((int)z > (int)q)
+                     {
+                         Console.WriteLine(array);
+                         Console.WriteLine(array2);
+                         break;
+                     }
+                     else
+                     {
+                         Console.WriteLine("");
+                         Console.WriteLine(array2);
+                         Console.WriteLine(array);
+                         break;
+                     }
+                    
+                 }
+                 break;
+             }
+
+
+
+
+
+
+                 
+         }
+     }
+     
+    
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //Патерн як організувати лабки
+           // RotateandSumm l1 = new RotateandSumm();
+          //  FoldandSumm l2 = new FoldandSumm();
+           // SieveOfEratosthenes l3 = new SieveOfEratosthenes();
+          //  CompareCharArrays l4 = new CompareCharArrays();
+
+
+        }
+    }
+}
---
+++ AndrushaTriesCsharp/AndrushaTriesCsharp/Program.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //1program
+        //Console.Write("Enter ur name ");
+        //String strangerName = Console.ReadLine();
+        //Console.WriteLine($"Hi , {strangerName}");
+
+        //2 
+        /*  int a, b, c;
+        Console.Write("Enter first number ");
+        a = Convert.ToInt32( Console.ReadLine());
+        Console.Write("Enter second number ");
+        b = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter third number ");
+        c = Convert.ToInt32(Console.ReadLine());
+        float average;
+        average = (a + b + c) / 3;
+        Console.WriteLine(average);
+        */
+
+
+
+        /*
+         * third program
+        float a, b, h, area ;
+
+        Console.Write("Enter a");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter b");
+        b = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter h");
+        h = Convert.ToInt32(Console.ReadLine());
+
+        area = (a + b) / 2 *h;
+        Console.WriteLine(area);
+        */
+
+        /*
+         * 4 program
+        int n, lastdigit;
+        Console.Write("Enter n");
+        n = Convert.ToInt32(Console.ReadLine());
+        lastdigit = n % 10;
+
+        Console.WriteLine(lastdigit);
+        */
+        /*
+        int n, number, nDigit;
+        Console.Write("Enter number");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter n");
+        n = Convert.ToInt32(Console.ReadLine());
+        nDigit = (number / 10 ^ (n - 1)) % 10;
+        Console.WriteLine(nDigit);
+       */
+
+
+        /*
+         * Working
+        int  number;
+        Console.Write("Enter number");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        if(number>20 && number % 2 != 0)
+        {
+            Console.WriteLine($"(number) is greater than 20 and odd ");
+        }
+        else
+        {
+            Console.WriteLine($"(number) is not greater than 20 or odd ");
+        }
+        */
+        /*allgood
+        int number;
+        Console.Write("Enter number");
+        number = Convert.ToInt32(Console.ReadLine());
+        if (number%9 == 0 && number%11 == 0 && number % 13 == 0)
+        {
+            Console.WriteLine("Working");
+        }
+    */
+        /*
+            int a, b, c;
+
+            Console.Write("Enter  1 number");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter  2 number");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter  3 number");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            int biggest=0;
+            if (a > b && a > c)
+                biggest = a;
+            if (b > a && b > c)
+                biggest = b;
+            if (c > a && c > b)
+                biggest = c;
+            Console.WriteLine(biggest);
+        */
+       /*
+        int number;
+        Console.Write("Enter  ");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        if(number > 7 && number<0)
+        {
+            Console.WriteLine("Eror");
+        }
+        switch (number)
+        {
+            case 1:
+                Console.WriteLine("Monday");
+                break;
+
+            case 2:
+                Console.WriteLine("Tuesday");
+                break;
+            case 3:
+                Console.WriteLine("Wednesday");
+                break;
+
+            case 4:
+                Console.WriteLine("Thursday");
+                break;
+
+            case 5:
+                Console.WriteLine("Friday");
+                break;
+
+            case 6:
+                Console.WriteLine("Saturday");
+                break;
+            case 7:
+                Console.WriteLine("Sunday");
+                break;
+            default:
+                Console.WriteLine("Not valid");
+                break;
+        }
+      */
+
+        /*
+         * 
+         */
+
+        /* WE HAVE DONE POG
+        int a, b, c, product=0;
+        Console.Write("Enter a ");
+        a = Convert.ToInt32(Console.ReadLine());
+        if (a < 0)
+        {
+            product++;
+        }
+    
+        Console.Write("Enter b ");
+        b = Convert.ToInt32(Console.ReadLine());
+        if (b < 0)
+        {
+            product+=1;
+        }
+        Console.Write("Enter c ");
+        c = Convert.ToInt32(Console.ReadLine());
+        if (c < 0)
+        {
+            product+=1;
+        }
+
+        switch (product)
+        {
+            case 0:
+                Console.WriteLine("Positive");
+                break;
+            case 1:
+                Console.WriteLine("Negative");
+                break;
+            case 2:
+                Console.WriteLine("Positive");
+                break;
+
+            case 3:
+                Console.WriteLine("Negative");
+                break;
+
+
+        }
+        */
+        /*
+         * DONT WRITE BIG NUMBERS
+        int n, factorial=1;
+
+        Console.Write("Enter n ");
+        n = Convert.ToInt32(Console.ReadLine());
+        for(int i=2; i<=n; i++)
+        {
+            factorial *= i;
+        }
+        Console.WriteLine(factorial);
+
+        */
+     
+      
+
+    }
+}
+
