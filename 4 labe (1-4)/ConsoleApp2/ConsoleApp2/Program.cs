2caf80f2bc2b1d90041b3b0a5d62e6a8545a971d
Furaw <44235585+Furaw@users.noreply.github.com>
2019-10-20 14:37:30 +0300

    Add files via upload
    4 лабароторна 1-4 завдання

---
+++ 4 labe (1-4)/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float length = float.Parse(Console.ReadLine());
+            float width = float.Parse(Console.ReadLine());
+            float height = float.Parse(Console.ReadLine());
+
+            Box box = new Box(length, width, height);
+
+            Console.WriteLine("Surface " + box.getSurfacearea());
+            Console.WriteLine("Lateral Surface -" + box.getLateralSurfaceArea());
+            Console.WriteLine("Volume - "+ box.getVolume());
+        }
+    }
+}
---
+++ 4 labe (1-4)/ConsoleApp4/ConsoleApp4/Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Product
+    {
+        private string name;
+        private float cost;
+
+        public Product(string name, float cost)
+        {
+            this.Name = name;
+            this.Cost = cost;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            private set
+            {
+                this.name = value;
+            }
+        }
+
+        public float Cost
+        {
+            get
+            {
+                return this.cost;
+            }
+
+            private set
+            {
+                this.cost = value;
+            }
+        }
+    }
+}
