d65bccf9534465a52d9b7585b76875b05bc64f99
Furaw <44235585+Furaw@users.noreply.github.com>
2019-12-01 16:16:09 +0200

    Add files via upload

---
+++ 9-1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var box1 = new Box<int>(123);
+        var box2 = new Box<string>("TextText");
+
+        Console.WriteLine(box1);
+        Console.WriteLine(box2);
+    }
+}
---
+++ 9-4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var boxes = new List<Box<int>>();
+
+        int counter = int.Parse(Console.ReadLine());
+
+        for(int i=0; i<counter; i++)
+        {
+            int value = int.Parse(Console.ReadLine());
+            boxes.Add(new Box<int>(value));
+        }
+
+        int[] indx = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+
+        Swap(boxes, indx[0], indx[1]);
+
+        foreach(var box in boxes)
+        {
+            Console.WriteLine(box);
+        }
+    }
+
+    static void Swap<T>(IList<Box<T>> list, int index1, int index2)
+    {
+        Box<T> temp = list[index1];
+        list[index1] = list[index2];
+        list[index2] = temp;
+    }
+}
